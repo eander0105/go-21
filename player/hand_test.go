e6650400ecd9d885dfe9933468526cbf26d4f066
Emil Andersson <eander0105@gmail.com>
2025-02-23 20:34:33 +0100

    hand test

--- player/hand_test.go
+++ player/hand_test.go
@@ -3,40 +3,149 @@ package player_test
 import (
 	"testing"
 
-	c "github.com/eander0105/go-21/card"
+	. "github.com/eander0105/go-21/card"
 	. "github.com/eander0105/go-21/player"
 )
 
+// Utils
+func flipAllCards(h *Hand) {
+	for i := range h.Cards {
+		h.Cards[i].Flip()
+	}
+}
+
+// Tests
 func TestBasicHandValues(t *testing.T) {
 	hand := Hand{
-		Cards: []c.Card{
-			c.NewCard(c.Hearts, c.King),
-			c.NewCard(c.Diamonds, c.King),
+		Cards: []Card{
+			NewCard(Hearts, King),
+			NewCard(Diamonds, King),
 		},
 	}
+	flipAllCards(&hand)
 
 	if hand.Value() != 20 {
 		t.Errorf("Expected 20, got %d", hand.Value())
 	}
 
 	hand = Hand{
-		Cards: []c.Card{
-			c.NewCard(c.Hearts, c.Two),   // 2
-			c.NewCard(c.Hearts, c.Three), // 3
-			c.NewCard(c.Hearts, c.Four),  // 4
-			c.NewCard(c.Hearts, c.Five),  // 5
-			c.NewCard(c.Hearts, c.Six),   // 6
-			c.NewCard(c.Hearts, c.Seven), // 7
-			c.NewCard(c.Hearts, c.Eight), // 8
-			c.NewCard(c.Hearts, c.Nine),  // 9
-			c.NewCard(c.Hearts, c.Ten),   // 10
-			c.NewCard(c.Hearts, c.Jack),  // 10
-			c.NewCard(c.Hearts, c.Queen), // 10
-			c.NewCard(c.Hearts, c.King),  // 10 = 74
+		Cards: []Card{
+			NewCard(Hearts, Two),   // 2
+			NewCard(Hearts, Three), // 3
+			NewCard(Hearts, Four),  // 4
+			NewCard(Hearts, Five),  // 5
+			NewCard(Hearts, Six),   // 6
+			NewCard(Hearts, Seven), // 7
+			NewCard(Hearts, Eight), // 8
+			NewCard(Hearts, Nine),  // 9
+			NewCard(Hearts, Ten),   // 10
+			NewCard(Hearts, Jack),  // 10
+			NewCard(Hearts, Queen), // 10
+			NewCard(Hearts, King),  // 10 = 84
+		},
+	}
+	flipAllCards(&hand)
+
+	if hand.Value() != 84 {
+		t.Errorf("Expected 74, got %d", hand.Value())
+	}
+}
+
+func TestIsSoft(t *testing.T) {
+	hand := Hand{
+		Cards: []Card{
+			NewCard(Hearts, Ace),
+			NewCard(Diamonds, King),
+		},
+	}
+	flipAllCards(&hand)
+
+	if !hand.IsSoft() {
+		t.Errorf("Expected true, got false")
+	}
+
+	hand = Hand{
+		Cards: []Card{
+			NewCard(Hearts, Two),   // 2
+			NewCard(Hearts, Three), // 3
+			NewCard(Hearts, Four),  // 4
+			NewCard(Hearts, Five),  // 5
+			NewCard(Hearts, Six),   // 6
+			NewCard(Hearts, Seven), // 7
+			NewCard(Hearts, Eight), // 8
+			NewCard(Hearts, Nine),  // 9
+			NewCard(Hearts, Ten),   // 10
+			NewCard(Hearts, Jack),  // 10
+			NewCard(Hearts, Queen), // 10
+			NewCard(Hearts, King),  // 10
+			NewCard(Hearts, Ace),   // 11
+		},
+	}
+	flipAllCards(&hand)
+
+	if hand.IsSoft() {
+		t.Errorf("Expected false, got true")
+	}
+}
+
+func TestFlipAllCards(t *testing.T) {
+	hand := Hand{
+		Cards: []Card{
+			NewCard(Hearts, Ace),
+			NewCard(Diamonds, King),
+			NewCard(Clubs, Queen),
 		},
 	}
 
-	if hand.Value() != 74 {
-		t.Errorf("Expected 74, got %d", hand.Value())
+	for _, card := range hand.Cards {
+		if card.FaceUp {
+			t.Errorf("Expected card to be face down, but it's face up")
+		}
+	}
+
+	flipAllCards(&hand)
+
+	for _, card := range hand.Cards {
+		if !card.FaceUp {
+			t.Errorf("Expected card to be face up, but it's face down")
+		}
+	}
+}
+
+func TestCalculateHandValue(t *testing.T) {
+	hand := Hand{
+		Cards: []Card{
+			NewCard(Hearts, Ace),
+			NewCard(Diamonds, King),
+		},
+	}
+	flipAllCards(&hand)
+
+	value := hand.Value()
+	if value != 21 {
+		t.Errorf("Expected hand value to be 21, but it's %d", value)
+	}
+	if !hand.IsSoft() {
+		t.Errorf("Expected hand to be soft, but it's not")
+	}
+}
+
+func TestCalculateHandValueMultipleCards(t *testing.T) {
+	hand := Hand{
+		Cards: []Card{
+			NewCard(Hearts, Ace),
+			NewCard(Diamonds, King),
+			NewCard(Clubs, Queen),
+			NewCard(Spades, Jack),
+		},
+	}
+	flipAllCards(&hand)
+
+	value := hand.Value()
+	if value != 31 {
+		t.Errorf("Expected hand value to be 31, but it's %d", value)
+	}
+	if hand.IsSoft() {
+		t.Errorf("Expected hand to be hard, but it's soft")
 	}
 }
